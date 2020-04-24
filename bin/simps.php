441e3ce8833c43573746248ee3f205998c8b8d2e
sy-records <52o@qq52o.cn>
2020-04-24 10:22:32 +0800

    update hook

--- bin/simps.php
+++ bin/simps.php
@@ -6,10 +6,12 @@ ini_set('display_startup_errors', 'on');
 
 error_reporting(E_ALL);
 
-! defined('BASE_PATH') && define('BASE_PATH', dirname(__DIR__, 1));
-! defined('SWOOLE_HOOK_FLAGS') && define('SWOOLE_HOOK_FLAGS', SWOOLE_HOOK_ALL);
-! defined('CONFIG_PATH') && define('CONFIG_PATH', dirname(__DIR__) . '/config/');
+!defined('BASE_PATH') && define('BASE_PATH', dirname(__DIR__, 1));
+!defined('SWOOLE_HOOK_FLAGS') && define('SWOOLE_HOOK_FLAGS', SWOOLE_HOOK_ALL);
+!defined('CONFIG_PATH') && define('CONFIG_PATH', dirname(__DIR__) . '/config/');
 
 require BASE_PATH . '/vendor/autoload.php';
+
+\Swoole\Runtime::enableCoroutine(SWOOLE_HOOK_FLAGS);
 
 Simps\Application::run();
